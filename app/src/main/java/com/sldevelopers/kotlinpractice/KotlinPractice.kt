5c7d5cd1724622b92f0d81dbdf3c192c90f9d7cd
Paul-Rajeevan_Nimalarajah <paulrajeevannimal@gmail.com>
2025-12-14 21:58:20 +0530

    val vs var

--- app/src/main/java/com/sldevelopers/kotlinpractice/KotlinPractice.kt
+++ app/src/main/java/com/sldevelopers/kotlinpractice/KotlinPractice.kt
@@ -1,10 +1,17 @@
 package com.sldevelopers.kotlinpractice
 
 fun main() {
-    println("Hello world")
+    //println("Hello world")
     val myList: List<Int> = listOf(4,8,16,32)
-    println(myList.get(myList.size-1))
-    println(myList[myList.size-1])
-    println(myList.last())
-    println(myList[myList.lastIndex])
+    //println(myList.get(myList.size-1))
+    //println(myList[myList.size-1])
+    //println(myList.last())
+    //println(myList[myList.lastIndex])
+
+    // val vs var
+    var otherfriends = mutableListOf("Mark","May")
+    //otherfriends.add("Paul")
+    println(otherfriends)
+
+
 }
