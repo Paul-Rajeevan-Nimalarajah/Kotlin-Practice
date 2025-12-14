7e6bc36809de5c56e388b153dd4d071820f6c7a0
Paul-Rajeevan_Nimalarajah <paulrajeevannimal@gmail.com>
2025-12-14 19:09:48 +0530

    first commit

---
+++ app/src/main/java/com/sldevelopers/kotlinpractice/KotlinPractice.kt
@@ -0,0 +1,10 @@
+package com.sldevelopers.kotlinpractice
+
+fun main() {
+    println("Hello world")
+    val myList: List<Int> = listOf(4,8,16,32)
+    println(myList.get(myList.size-1))
+    println(myList[myList.size-1])
+    println(myList.last())
+    println(myList[myList.lastIndex])
+}
