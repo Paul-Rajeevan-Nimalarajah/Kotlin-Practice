b2cd4a027ad18b3b8039e6f1bf028764113e4818
Paul-Rajeevan_Nimalarajah <paulrajeevannimal@gmail.com>
2025-12-21 00:16:03 +0530

    null

---
+++ app/src/main/java/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    //println("Hello World!")
+    //val x: Int =5
+    //val Y: Boolean = true
+    //println(Y)
+    //val x = 17
+    //val y = 9
+    //println(x/y)
+    //val Number = x==y
+    //println(Number)
+    /*val x = 12
+    val y = 14
+    var areEven = (x%2==0 || (y%2==0 && x+y==25))
+    println(areEven)*/
+    /*println("Enter a number")
+    val input = readln()
+    val number = input.toIntOrNull() ?:1
+    val isEven = number%2==0
+    println(isEven)*/
+    /*val input = readln()
+    val intInput = input.toIntOrNull()?.inc()
+    println(intInput)*/
+
+}
